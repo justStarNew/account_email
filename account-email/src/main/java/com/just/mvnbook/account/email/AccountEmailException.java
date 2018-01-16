ce567f548cdcf76f5e9b444846738b3c791d9ced
zhangcai <1159593866@qq.com>
2018-01-16 23:17:16 +0800

    初始化提交

---
+++ account-email/src/main/java/com/just/mvnbook/account/email/AccountEmailException.java
@@ -0,0 +1,11 @@
+package com.just.mvnbook.account.email;
+
+import javax.mail.MessagingException;
+
+public class AccountEmailException extends Exception{
+
+	public void AccountEmailException(String string, MessagingException e) {
+		// TODO Auto-generated constructor stub
+	}
+	 
+}
---
+++ account-email/src/main/java/com/just/mvnbook/account/email/AccountEmailService.java
@@ -0,0 +1,8 @@
+package com.just.mvnbook.account.email;
+
+public interface AccountEmailService {
+
+	void sendMail(String to,String subject,String htmlText )
+	 	throws AccountEmailException;
+	
+}
